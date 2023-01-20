8c7e66ee490801230f6003c7667128ae1b2a3260
Matt Lee <matthew.d.lee@hotmail.co.uk>
2023-01-20 21:32:44 +0000

    Minor Changes

--- Screens/CharacterScreen.js
+++ Screens/CharacterScreen.js
@@ -165,7 +165,6 @@ const styles = StyleSheet.create({
   },
   ScrollView: {
     width: '100%',
-    marginTop: 20,
   },
   name: {
     marginTop: 60,
@@ -182,6 +181,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 5,
     elevation: 5,
+    marginBottom: 20
   },
   healthArmour: {
     alignSelf: 'center', 
